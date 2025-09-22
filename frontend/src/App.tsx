7b524bfb85db9755145ca6bf4b35185d8bc2c206
The-Best-Codes <bestcodes.official@gmail.com>
2025-09-22 18:52:30 -0500

    make name in header be a link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "next-themes";
-import { Route, BrowserRouter as Router, Routes } from "react-router";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router";
 import EnterCodePage from "./pages/EnterCodePage";
 import HomePage from "./pages/HomePage";
 import InstructionsPage from "./pages/InstructionsPage";
@@ -12,7 +12,9 @@ function App() {
       <Router>
         <div className="min-h-[100svh] bg-background text-foreground flex items-center justify-center p-4 pt-16 relative">
           <header className="absolute top-0 left-0 w-full bg-accent flex items-center justify-start p-4 h-16">
-            <h1 className="text-2xl font-bold">BCShare</h1>
+            <h1 className="text-2xl font-bold">
+              <Link to="/">BCShare</Link>
+            </h1>
           </header>
           <Routes>
             <Route path="/" element={<HomePage />} />
