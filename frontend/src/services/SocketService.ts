8961fb5d901d33be6e1fdaceb80e39e6c815fbb3
The-Best-Codes <bestcodes.official@gmail.com>
2025-09-23 10:54:13 -0500

    mroe stun servers

--- frontend/src/services/SocketService.ts
+++ frontend/src/services/SocketService.ts
@@ -111,7 +111,11 @@ class SocketService {
   private createPeerConnection() {
     try {
       this.peerConnection = new RTCPeerConnection({
-        iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+        iceServers: [
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun1.l.google.com:19302" },
+          { urls: "stun:stun2.l.google.com:19302" }
+        ],
       });
 
       this.peerConnection.onicecandidate = (event) => {
