50cd66e839f0f7a02143eb2f453acdccb90a00f8
The-Best-Codes <bestcodes.official@gmail.com>
2025-09-23 11:06:08 -0500

    chore: fmt

--- frontend/src/services/SocketService.ts
+++ frontend/src/services/SocketService.ts
@@ -114,7 +114,7 @@ class SocketService {
         iceServers: [
           { urls: "stun:stun.l.google.com:19302" },
           { urls: "stun:stun1.l.google.com:19302" },
-          { urls: "stun:stun2.l.google.com:19302" }
+          { urls: "stun:stun2.l.google.com:19302" },
         ],
       });
 
